fd9b757329e56cc1e2fa92fc3fa91c03a656743c
Lorena <minfante4650@universidadean.edu.co>
2018-02-27 20:48:46 -0500

    inicio

---
+++ scripts/logica.js
@@ -0,0 +1,8 @@
+//Saludo en la consola
+console.log("Hola mundo");
+
+//Sludo en una alerta
+alert("¡Hola mundo!");
+
+//Escribe en la pantalla
+document.write("<h1>Hallo Welt! Ich bin Lorena</h1>");
